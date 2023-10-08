c4a10956ce07ee8c6272dba62db83afcce15c43c
IlDojoWebsite <146979647+IlDojoWebsite@users.noreply.github.com>
2023-10-09 00:38:56 +0200

    Update contact.php
    changed mail to asd.ildojo@gmail.com

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "francescoiaria1202@gmail.com";
+    $email_to = "asd.ildojo@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
